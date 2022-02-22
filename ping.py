dd20a43e1a55c6fe001ca4410abe6ec2ef200710
FURY 999 <76838573+fury999io@users.noreply.github.com>
2022-02-22 12:19:39 +0530

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,16 @@
+# Developed by fury999
+# github.com/fury999io
+# pyautogui module must be installed
+# After running the code, move the cursor immediately to the message box of the group chat. Wait till the last person of the group gets mentioned. Then move your mouse pointer towards the corner of the screen, the program would stop
+import pyautogui, time
+  
+time.sleep(2)
+n=0
+while True:
+	i=0
+	pyautogui.typewrite("@")
+	while i<=n:
+		pyautogui.press("down")
+		i=i+1
+	pyautogui.press("enter")
+	n=n+1
