7d5550ce597e3d5d3c4d1acc0306402a78c56573
FURY 999 <fury999io@outlook.com>
2023-04-04 17:40:29 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,6 +1,6 @@
 # Developed by fury999
 # github.com/fury999io
-# After running the code, move the cursor immediately to the message box of the group chat. Wait till the last person of the group gets mentioned. Then move your mouse pointer towards the corner of the screen, the program would stop
+# After running the code, move the cursor immediately to the message box of the group chat. Wait till the last person of the group gets mentioned. Then move your mouse pointer towards a corner of the screen, the program would terminate.
 import pyautogui, time
 time.sleep(2)
 pyautogui.typewrite("@")
