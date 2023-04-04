2ec68087ccbfbe1abf54811a7d9e349afa327210
FURY 999 <fury999io@outlook.com>
2023-04-04 17:39:38 +0530

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,6 +3,9 @@
 # After running the code, move the cursor immediately to the message box of the group chat. Wait till the last person of the group gets mentioned. Then move your mouse pointer towards the corner of the screen, the program would stop
 import pyautogui, time
 time.sleep(2)
+pyautogui.typewrite("@")
+pyautogui.press("enter")
+pyautogui.hotkey('shift', 'enter')
 while True:
 	pyautogui.typewrite("@")
 	pyautogui.press("down")
